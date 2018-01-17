41cdc15698058b9dcf48ecbfc65010770ea8e3f4
play4fun <play4fun@foxmail.com>
2018-01-17 12:46:46 +0800

    在头顶画一个圆

--- py/ClosestThing1.py
+++ py/ClosestThing1.py
@@ -45,14 +45,34 @@ if __name__ == "__main__":
             # get a frame from depth sensor
             depth = get_depth()
             # frame[depth < minThresh] = (255, 255, 255)  # (0, 0, 0)
-            frame[depth < minThresh] = (255, 0, 0)  # (0, 0, 0)
+            mindep = depth < minThresh
+            frame[mindep] = (0, 0, 0)  # (255, 0, 0)  #
             # frame[depth > maxThresh] = (0, 0, 0)  # (255, 255, 255)
-            frame[depth > maxThresh] = (0, 0, 255)  # (255, 255, 255)
+            maxdep = depth > maxThresh
+            frame[maxdep] = (255, 255, 255)  # (0, 0, 255)  #
             # print('depth', type(depth), depth.shape)#<class 'numpy.ndarray'> (480, 640)
+            try:  # 在头顶画一个圆
+                rs = np.where(np.logical_and(depth >= minThresh, depth <= maxThresh) == True)
+                # print(rs)
+                l1 = [(x, y) for x, y in zip(rs[0], rs[1])]
+                # l2 = sorted(l1, key=lambda d: d[1])
+                l3 = sorted(l1)
+                # point = rs[0][0], rs[1][0]
+                # point = l2[0]
+                # print('point:', point)
+                # cv2.circle(frame, center=(point[1],point[0]), radius=10, color=(0, 255, 0), thickness=-1)#绿色
+
+                p2 = (l3[0][1], l3[0][0])
+                print('point:', p2)
+                cv2.circle(frame, center=p2, radius=10, color=(255, 0, 0), thickness=-1)  # 蓝色
+            except Exception as e:
+                print(e)
+                pass
+
             # display RGB image
             cv2.imshow('RGB image', frame)
             # display depth image
-            cv2.imshow('Depth image', depth.astype(np.uint8))
+            # cv2.imshow('Depth image', depth.astype(np.uint8))
 
             # quit program when 'esc' key is pressed
             k = cv2.waitKey(5)  # & 0xFF
