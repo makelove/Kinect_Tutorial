e4e847960495cc14786993f8eaa426eb8cb501a5
play4fun <play4fun@foxmail.com>
2018-01-19 19:36:26 +0800

    timestamp

--- py/depth_test1.py
+++ py/depth_test1.py
@@ -17,12 +17,15 @@ import pickle
 if __name__ == "__main__":
     print('start')
     while True:
-        array, _ = freenect.sync_get_depth()
+        array, timestamp = freenect.sync_get_depth()
         depth = array.astype(np.uint8)
         cv2.imshow('Depth image', depth)
 
+        # color_img = cv2.cvtColor(depth,cv2.COLOR_GRAY2RGB)#没用
+        # cv2.imshow('color_img', color_img)
+
         k = cv2.waitKey(5)  # & 0xFF
         if k == 27:
-            with open('depth_ndarray-1', 'wb') as f:
-                pickle.dump(array, f)
+            # with open('depth_ndarray-1', 'wb') as f:#保存下来，为了以后在ipython内调试
+            #     pickle.dump(array, f)
             break
