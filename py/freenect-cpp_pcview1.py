f02eb3ac8a0027f9eff3ad96c40207907f5a5368
play4fun <play4fun@foxmail.com>
2018-01-25 00:06:42 +0800

    createDevice

--- py/freenect-cpp_pcview1.py
+++ py/freenect-cpp_pcview1.py
@@ -11,3 +11,44 @@ freenect-cpp_pcview 用python实现
 """
 
 
+
+
+def main():
+    device = & freenect.createDevice < MyFreenectDevice > (0);
+    device->startVideo();
+    device->startDepth();
+
+    glutInit( & argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(0, 0);
+
+    window = glutCreateWindow("LibFreenect");
+    glClearColor(0.45
+    f, 0.45
+    f, 0.45
+    f, 0.0
+    f);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_GREATER, 0.0
+    f);
+
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(50.0, 1.0, 900.0, 11000.0);
+
+    glutDisplayFunc( & DrawGLScene);
+    glutIdleFunc( & idleGLScene);
+    glutReshapeFunc( & resizeGLScene);
+    glutKeyboardFunc( & keyPressed);
+    glutMotionFunc( & mouseMoved);
+    glutMouseFunc( & mouseButtonPressed);
+
+    printInfo();
+
+    glutMainLoop();
+
+if __name__ == '__main__':
+    main()
