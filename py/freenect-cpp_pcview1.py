b46b9e62ad4456d5690f089da513d4b9a2dbdb40
play4fun <play4fun@foxmail.com>
2018-01-25 11:16:01 +0800

    glVertex3f

--- py/freenect-cpp_pcview1.py
+++ py/freenect-cpp_pcview1.py
@@ -22,6 +22,13 @@ except:
     print(''' Error: PyOpenGL not installed properly ''')
     sys.exit()
 
+mx = -1
+my = -1  # Prevous mouse coordinates
+anglex = 0
+angley = 0  # Panning angles
+zoom = 1  # Zoom factor
+color = True  # Flag to indicate to use of color in the cloud
+
 
 def printInfo():
     txt = "\nAvailable Controls:" + "===================" + "Rotate       :   Mouse Left Button" + "Zoom         :   Mouse Wheel" + "Toggle Color :   C" + "Quit         :   Q or Esc\n"
@@ -30,17 +37,87 @@ def printInfo():
 
 
 def DrawGLScene():  # 主要功能
-    array, timestamp = freenect.sync_get_depth()
-    array >>= 2  # 只需要这个
-
+    # 深度相机
+    depth, timestamp = freenect.sync_get_depth()
+    depth >>= 2  # 只需要这个
+    # RGB相机
     rgbarray, timestamp2 = freenect.sync_get_video()
     rgb = cv2.cvtColor(rgbarray, cv2.COLOR_RGB2BGR)
 
-    pass
+    #
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    glPointSize(1.0)
+
+    glBegin(GL_POINTS)
+
+    col3 = glColor3ub(255, 255, 255)
+    # if (!color) glColor3ub(255, 255, 255):
+    if not color and col3:
+        # for (int i = 0; i < 480 * 640; ++i):
+        for i in range(480 * 640):
+
+            if color:
+                glColor3ub(rgb[3 * i + 0],  # R
+                           rgb[3 * i + 1],  # G
+                           rgb[3 * i + 2])  # B
+
+                f = 595.0
+                # Convert from image plane  coordinates to world  coordinates
+
+                glVertex3f((i % 640 - (640 - 1) / 2.) * depth[i] / f,  # X = (x - cx) * d / fx
+                           (i / 640 - (480 - 1) / 2.) * depth[i] / f,  # Y = (y - cy) * d / fy
+                           depth[i])  # Z = d
+
+    glEnd()
+
+    # Draw world coordinate frame
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    glColor3ub(255, 0, 0)  # X - axis
+    glVertex3f(0, 0, 0)
+    glVertex3f(50, 0, 0)
+    glColor3ub(0, 255, 0)  # Y - axis
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 50, 0)
+    glColor3ub(0, 0, 255)  # Z - axis
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 50)
+    glEnd()
+
+    # Place the camera
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glScalef(zoom, zoom, 1)
+    gluLookAt(-7 * anglex, -7 * angley, -1000.0,
+              0.0, 0.0, 2000.0,
+              0.0, -1.0, 0.0)
+
+    glutSwapBuffers()
+
 
 def idleGLScene():
+    glutPostRedisplay()
 
-    pass
+
+def resizeGLScene(width: int, height: int):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(50.0, float(width / height), 900.0, 11000.0)
+
+    glMatrixMode(GL_MODELVIEW)
+
+
+def mouseMoved(x: int, y: int):
+    global mx, my, anglex, angley
+
+    if mx >= 0 and my >= 0:
+        anglex += x - mx
+        angley += y - my
+
+    mx = x
+    my = y
 
 
 def main():
@@ -55,22 +132,23 @@ def main():
     glutInitWindowSize(640, 480)
     glutInitWindowPosition(0, 0)
 
-    glutCreateWindow("LibFreenect")
+    glutCreateWindow("freenect-cpp_pcview")
     glClearColor(0.45, 0.45, 0.45, 0.0)
 
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_ALPHA_TEST)
     glAlphaFunc(GL_GREATER, 0.0)
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_PROJECTION)
     gluPerspective(50.0, 1.0, 900.0, 11000.0)
 
     glutDisplayFunc(DrawGLScene)  # 最重要是这个函数
     glutIdleFunc(idleGLScene)
+
     # TODO
-    # glutReshapeFunc(resizeGLScene)
+    glutReshapeFunc(resizeGLScene)
     # glutKeyboardFunc(keyPressed)
-    # glutMotionFunc(mouseMoved)
+    glutMotionFunc(mouseMoved)
     # glutMouseFunc(mouseButtonPressed)
 
     printInfo()
